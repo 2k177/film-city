bc5934ef48e79bf47d61cb921656ffa793aaf2c2
Danusha U <danuu@qti.qualcomm.com>
2023-04-13 11:18:04 +0530

    update 2 way binding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [searchStr, setSearchStr] = useState('');
+  console.log(searchStr);
+  const onSearchInputChange = ev => {
+    // console.log(ev.target.value);
+    setSearchStr(ev.target.value);
+  };
+
+  return (
+    <div>
+      <div>{searchStr}</div>
+      <button
+        type="button"
+        onClick={() => {
+          setSearchStr('Danusha');
+        }}
+      >
+        Click me
+      </button>
+      <input type="text" value={searchStr} onChange={onSearchInputChange} />
+    </div>
+  );
 };
 
 export default Home;
