bb3007ec9d9d861b4a0c680a5ffaa93375901118
Danusha U <danuu@qti.qualcomm.com>
2023-04-13 12:59:49 +0530

    Integrate tv api using fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,27 @@ import { useState } from 'react';
 
 const Home = () => {
   const [searchStr, setSearchStr] = useState('');
-  console.log(searchStr);
+
   const onSearchInputChange = ev => {
     // console.log(ev.target.value);
     setSearchStr(ev.target.value);
   };
 
+  const onSearch = async ev => {
+    ev.preventDefault();
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
+
   return (
     <div>
-      <div>{searchStr}</div>
-      <button
-        type="button"
-        onClick={() => {
-          setSearchStr('Danusha');
-        }}
-      >
-        Click me
-      </button>
-      <input type="text" value={searchStr} onChange={onSearchInputChange} />
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">search</button>
+      </form>
     </div>
   );
 };
