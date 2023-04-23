71731129460a04aecc75c406988d9dd19055988a
Danusha U <danuu@qti.qualcomm.com>
2023-04-23 18:25:26 +0530

    Add custom icons and updated manifest.json

--- src/components/shows/ShowMainData.jsx
+++ src/components/shows/ShowMainData.jsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import { StarIcon } from '../common/StarIcon';
+import NotFoundImgSrc from '../../lib/not-found-image.png';
 
 const ShowMainData = ({ image, name, rating, summary, genres }) => {
   return (
     <MainDataWrapper>
-      <img src={image ? image.original : '/not-found-image.png'} alt={name} />
+      <img src={image ? image.original : NotFoundImgSrc} alt={name} />
 
       <DataSection>
         <Headline>
